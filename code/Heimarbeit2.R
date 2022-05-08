59cf0cca71366a5ca57aaa9ed3ffa8d64534a4f8
TobiasHoesli <tobias.hoesli@hotmail.com>
2022-05-08 13:02:30 +0200

    small unstashed changes from Heimarbeit2

--- code/Heimarbeit2.R
+++ code/Heimarbeit2.R
@@ -1,16 +1,43 @@
-## ----setup, include=FALSE---------------------------------------------------------------------------------------------------------------------
+## ----setup, include=FALSE-------------------------------------------------------------------
 knitr::opts_chunk$set(echo = TRUE)
 
 
-## ----set up of packages, include=FALSE--------------------------------------------------------------------------------------------------------
+## ----set up of packages, include=FALSE------------------------------------------------------
 # install.packages("statnet")
 library("RColorBrewer")
 library("statnet")
 library("scales")
 library("formatR")
 
+library("knitr")
+library("readxl")
+library("tidyr")
+library("kableExtra")
+library("dplyr")
 
-## ----set up of edge list , tidy=TRUE----------------------------------------------------------------------------------------------------------
+
+
+
+## ----Visualize the 20 teams in table, echo=FALSE--------------------------------------------
+team_table <- read_excel("data/Edge_List_Transfers.xlsx",
+    sheet = "Teams")
+
+
+team_table_sl <- team_table %>% filter(Liga == "Super League") %>% select(Team,Kuerzel)
+team_table_cl <- team_table %>% filter(Liga == "Challenge League") %>% select(Team,Kuerzel)
+
+
+kable(team_table_sl, caption= "Super League Teams (CL)", booktabs = T)  %>%
+  kable_styling(latex_options = "HOLD_position")
+
+kable(team_table_cl, caption= "Challenge League Teams (CL)", booktabs = T)  %>%
+  kable_styling(latex_options = "HOLD_position")
+
+
+
+
+
+## ----defining of edge list, tidy=TRUE, include=FALSE----------------------------------------
 edge_list <- read.csv("data/Edge_List_Values.csv", sep = ";")
 
 # Ertsellen der Kanten-Liste aufgrund der gesammelten Daten
@@ -21,8 +48,10 @@ transfer_net <- network(netmat2, matrix.type = "edgelist", directed = TRUE)
 
 # Knoten benennen
 #network.vertex.names(transfer_net) <- c(
-#    "BSC Young Boys", "FC Basel", "Servette FC", "FC Lugano", "FC Luzern", "Lausanne-Sport", "FC St. Gallen", "FC Zürich", "FC Sion", "Grasshoppers",
-#    "FC Vaduz", "FC Thun", "Stade-Lausanne", "FC Schaffhausen", "FC Aarau", "FC Winterthur", "FC Wil 1900", "SC Kriens", "Neuchâtel Xamax", "Yverdon #Sport"
+#    "BSC Young Boys", "FC Basel", "Servette FC", "FC Lugano", "FC Luzern",
+# "Lausanne-Sport", "FC St. Gallen", "FC Zürich", "FC Sion", "Grasshoppers",
+#    "FC Vaduz", "FC Thun", "Stade-Lausanne", "FC Schaffhausen", "FC Aarau",
+# "FC Winterthur", "FC Wil 1900", "SC Kriens", "Neuchâtel Xamax", "Yverdon #Sport"
 #)
 
 # Knoten Acronyms 
@@ -72,32 +101,35 @@ set.edge.attribute(transfer_net, "type", type)
 # Knoten-Attribut "alldeg" definieren (abgekürzte Variante)
 transfer_net %v% "alldeg" <- degree(transfer_net)
 
-summary(transfer_net)
+summary(transfer_net, print.adj = FALSE)
 
 
 
 
-## ----first few plots, echo=TRUE , tidy=TRUE, tidy.opts = list(blank = FALSE, width.cutoff = 60)-----------------------------------------------
+## ----first few layout plots, echo=FALSE , tidy=TRUE, tidy.opts = list(blank = FALSE, width.cutoff = 60)----
 
 my_pal = c("#2ca25f","#de2d26")
 league_cat = as.factor(get.vertex.attribute(transfer_net,"league"))
 
 edge_pal = c("#a8ddb5")
 
+set.seed(5)
 par(mar= c(2,0,2,0),mfrow=c(2,3))
 gplot(transfer_net, vertex.col = my_pal[league_cat], displaylabels = TRUE, label.cex = 0.75, edge.col = edge_pal, edge.lwd = 0.5, mode = "circle", main = "circle", label.col = my_pal[league_cat])
 gplot(transfer_net, vertex.col = my_pal[league_cat], displaylabels = TRUE, label.cex = 0.75, edge.col = edge_pal, edge.lwd = 0.5, mode = "kamadakawai", main = "kamadakawai", label.col = my_pal[league_cat])
 gplot(transfer_net, vertex.col = my_pal[league_cat], displaylabels = TRUE, label.cex = 0.75, edge.col = edge_pal, edge.lwd = 0.5, mode = "random", main = "random", label.col = my_pal[league_cat])
 gplot(transfer_net, vertex.col = my_pal[league_cat], displaylabels = TRUE, label.cex = 0.75, edge.col = edge_pal, edge.lwd = 0.5, mode = "fruchtermanreingold", main = "fruchtermanreingold", label.col = my_pal[league_cat])
-gplot(transfer_net, vertex.col = my_pal[league_cat], displaylabels = TRUE, label.cex = 0.75, edge.col = edge_pal, edge.lwd = 0.5, mode = "fruchtermanreingold", main = "spring", label.col = my_pal[league_cat])
-gplot(transfer_net, vertex.col = my_pal[league_cat], displaylabels = TRUE, label.cex = 0.75, edge.col = edge_pal, edge.lwd = 0.5, mode = "fruchtermanreingold", main = "eigen", label.col = my_pal[league_cat])
+gplot(transfer_net, vertex.col = my_pal[league_cat], displaylabels = TRUE, label.cex = 0.75, edge.col = edge_pal, edge.lwd = 0.5, mode = "spring", main = "spring", label.col = my_pal[league_cat])
+gplot(transfer_net, vertex.col = my_pal[league_cat], displaylabels = TRUE, label.cex = 0.75, edge.col = edge_pal, edge.lwd = 0.5, mode = "eigen", main = "eigen", label.col = my_pal[league_cat])
 
 
 
-## ----compute betweenness, degree and closeness------------------------------------------------------------------------------------------------
+## ----compute betweenness, degree and closeness----------------------------------------------
 
 bet <-betweenness(transfer_net, gmode = "graph")
+summary(bet)
 bet
+
 
 deg <-degree(transfer_net, gmode = "graph")
 deg
@@ -108,31 +140,32 @@ cls
 
 
 
-## ----include spendings as quantitatives Knotenattribut----------------------------------------------------------------------------------------
+## ----visualization vertex attribute, echo=FALSE---------------------------------------------
 
 par(mar=c(0,0,0,0), mfrow=c(1,1))
 vertex_col <- c("#7fcdbb","#1d91c0")
 edge_col <- c("#9ebcda")
 
 
-
+set.seed(5)
 par(mar=c(1,0,1,0), mfrow=c(1,1))
 gplot(transfer_net, vertex.col = vertex_col[league_cat],
       displaylabels = TRUE,
       vertex.cex = deg/1.95,
-      edge.col= edge_col ,mode = "fruchtermanreingold", label.cex = 0.75, label.pos = 0
+      edge.col= edge_col ,mode = "fruchtermanreingold", label.cex = 0.75, label.pos = 0,
+      usecurve = TRUE
       )
 
 
 
-## ----Knotengrösse Anpassen gemäss betweenness , tidy=TRUE, tidy.opts = list(blank = FALSE, width.cutoff = 60)---------------------------------
+## ----Knotengrösse Anpassen gemäss betweenness , echo=FALSE, tidy=TRUE, tidy.opts = list(blank = FALSE, width.cutoff = 60)----
 
 par(mar=c(1,0,1,0), mfrow=c(1,1))
 linecol_pal <- c("#e0209d","#fa9fb5","#ffc74f")
 league_pal = c("#aa36eec4", "#f353de")
 type_cat <- as.factor(get.edge.attribute(transfer_net,"type"))
 
-set.seed(7)
+set.seed(8)
 gplot(transfer_net, vertex.col = league_pal[league_cat],
       displaylabels = TRUE,
       vertex.cex = sqrt(bet+1)/1.85,
@@ -152,19 +185,13 @@ legend("bottomright", legend= c("Challenge League", "Super League"),
 
 
 
-## ---------------------------------------------------------------------------------------------------------------------------------------------
-
-get.vertex.attribute(transfer_net,"vertex.names")
-
-
-class(transfer_net)
-
-
-
-
-## ----convert markdown to R Script-------------------------------------------------------------------------------------------------------------
+## ----convert markdown to R Script, echo=FALSE-----------------------------------------------
 
 #knitr::purl("Heimarbeit2.rmd")
 
+
+
+
+## ----ref.label=knitr::all_labels(), echo=TRUE, eval=TRUE, tidy=TRUE, tidy.opts = list(blank = FALSE, width.cutoff = 60)----
 
 
